d102d85dd7bc53da5f19afa6543dafaf59110dee
Andreas Reffstrup Rasmussen <arra@itu.dk>
2014-11-23 01:41:02 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,3 +55,74 @@ $(document).ready(function(){
   });
       
 });
+
+// ---------- Animated stroke under intro-3btn (company) -----------
+
+//  var underlineWidth = '10%';
+//
+//  $('#three-ways').mouseenter(function(){
+//    $('.intro-3btn-content').children().css('opacity', 1);
+//    $('#underline3').animate({width: underlineWidth}, 250, 'easeInOutQuint');
+//  }).mouseleave(function(){
+//    $('.intro-3btn-content').children().css('opacity', 0.9);
+//    $('#underline3').animate({width: "0"}, 150, 'easeOutQuint');
+//  });
+//  
+//  $('#others-use').mouseenter(function(){
+//    $('.intro-3btn-content').children().css('opacity', 1);
+//    $('#underline4').animate({width: underlineWidth}, 250, 'easeInOutQuint');
+//  }).mouseleave(function(){
+//    $('.intro-3btn-content').children().css('opacity', 0.9);
+//    $('#underline4').animate({width: "0"}, 150, 'easeOutQuint');  
+//  }); 
+//
+//  $('#see-stud-projects').mouseenter(function(){
+//    $('.intro-3btn-content').children().css('opacity', 1);
+//    $('#underline5').animate({width: underlineWidth}, 250, 'easeInOutQuint');
+//  }).mouseleave(function(){
+//    $('.intro-3btn-content').children().css('opacity', 0.9);
+//    $('#underline5').animate({width: "0"}, 150, 'easeOutQuint');  
+//  }); 
+
+// ---------- Animated box around intro-3btn (company) -----------
+
+  var underlineWidth = '90%';
+  var underlineHeight = '90%';
+  var easeWidth = 'easeInOutQuint';
+  var easeHeight = 'easeInOutQuint';
+
+  $('#three-ways').mouseenter(function(){
+    $(this).children().css('opacity', 1);
+    $('#underline3').css('opacity', 1);
+    $('#underline3').animate({width: underlineWidth}, 100, easeWidth);
+    $('#underline3').animate({height: underlineHeight}, 100, easeHeight);
+  }).mouseleave(function(){
+    $(this).children().css('opacity', 0.9);
+    $('#underline3').animate({width: '0'}, 1);
+    $('#underline3').animate({height: '0'}, 1);
+    $('#underline3').css('opacity', 0);
+  });
+
+  $('#others-use').mouseenter(function(){
+    $(this).children().css('opacity', 1);
+    $('#underline4').css('opacity', 1);
+    $('#underline4').animate({width: underlineWidth}, 100, easeWidth);
+    $('#underline4').animate({height: underlineHeight}, 100, easeHeight);
+  }).mouseleave(function(){
+    $(this).children().css('opacity', 0.9);
+    $('#underline4').animate({width: '0'}, 1);
+    $('#underline4').animate({height: '0'}, 1);
+    $('#underline4').css('opacity', 0);
+  });
+
+  $('#see-stud-projects').mouseenter(function(){
+    $(this).children().css('opacity', 1);
+    $('#underline5').css('opacity', 1);
+    $('#underline5').animate({width: underlineWidth}, 100, easeWidth);
+    $('#underline5').animate({height: underlineHeight}, 100, easeHeight);
+  }).mouseleave(function(){
+    $(this).children().css('opacity', 0.9);
+    $('#underline5').animate({width: '0'}, 1);
+    $('#underline5').animate({height: '0'}, 1);
+    $('#underline5').css('opacity', 0);
+  });
